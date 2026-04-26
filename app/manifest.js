f0beec4285e1ac75050b53470e1af76e10bd24b2
sweetsnao15-cpu <sweetsnao15@gmail.com>
2026-04-26 19:37:52 +0900

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -1,1 +1,24 @@
-
+// app/manifest.js
+export default function manifest() {
+  return {
+    name: 'for VAU',               // インストール時の名前
+    short_name: 'for VAU',         // アイコンの下に表示される名前
+    description: 'for VAU Communication App',
+    start_url: '/',
+    display: 'standalone',         // アプリのように全画面表示
+    background_color: '#000000',
+    theme_color: '#800000',        // テーマカラー
+    icons: [
+      {
+        src: '/icon-192.png',      // publicフォルダに配置した画像
+        sizes: '192x192',
+        type: 'Gemini_Generated_Image_dkgzo1dkgzo1dkgz.png',
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
