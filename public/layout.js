2b6937d046eb097a470f7dd4b6191197e687cee3
sweetsnao15-cpu <sweetsnao15@gmail.com>
2026-04-30 18:22:39 +0900

    Update layout.js

--- public/layout.js
+++ public/layout.js
@@ -23,11 +23,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja" className="h-full antialiased">
       <head>
-        {/* iOS用：最も確実なアイコン指定方法 */}
-        <link rel="apple-touch-icon" href="/icon-512.png" />
-        {/* 複数のサイズを指定する場合（予備） */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/icon-512.png" />
+        {/* ?v=2 を付けることで、スマホに残っている古いキャッシュを無視させます */}
+        <link rel="apple-touch-icon" href="/icon-512.png?v=2" />
+        <link rel="icon" href="/icon-512.png?v=2" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="for VAU" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
